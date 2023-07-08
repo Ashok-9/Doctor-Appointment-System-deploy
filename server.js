a4559e17ff65ea1a8f225503d98562f927415130
ashok-9 <ashokbommena9@gmail.com>
2023-07-09 04:42:32 +0530

    doctor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const path = require("path");
 //dotenv conig
 
 dotenv.config();
@@ -23,6 +23,14 @@ app.use(moragan("dev"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const port = process.env.PORT || 8000;
